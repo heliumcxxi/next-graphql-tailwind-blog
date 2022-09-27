99e52bf5e052d211d6a868bf6d00b551bf2dff0c
Heliumcxxi <Heliumcxxi@users.noreply.github.com>
2022-09-27 10:14:27 -0400

    updated feature bar

--- components/FeaturedPost.jsx
+++ components/FeaturedPost.jsx
@@ -3,6 +3,27 @@ import moment from 'moment'
 import Link from 'next/link'
 import { getFeaturedPosts } from '../services'
 import FeaturedPostCard from './FeaturedPostCard'
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css';
+
+const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 1024 },
+    items: 5,
+  },
+  desktop: {
+    breakpoint: { max: 1024, min: 768 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 768, min: 640 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 640, min: 0 },
+    items: 1,
+  },
+}
 
 const FeaturedPost = ({post}) => {
   const [featuredPost, setFeaturedPost] = useState([])
@@ -17,11 +38,13 @@ const FeaturedPost = ({post}) => {
   },[])
 
   return (
-    <div className='bg-white'>
-     
-      {dataLoaded && featuredPost.map((post) => {
-        <FeaturedPostCard post={post} />
-      })}
+    <div className='mb-4'>
+      <Carousel responsive={responsive} itemClass="px-4">
+        {dataLoaded && featuredPost.map((post, idx) => {
+        return (
+          <FeaturedPostCard key={idx} post={post}/>
+        )})} 
+      </Carousel>
     </div>
   )
 }
